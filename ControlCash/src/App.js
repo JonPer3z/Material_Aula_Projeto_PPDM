9a59c940c7fe185bff3e5ef096d8727fe0d81a1d
JonPer3z <perez.joaopedro@gmail.com>
2025-08-19 17:29:14 -0300

    CorFundo

--- ControlCash/src/App.js
+++ ControlCash/src/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from '../Screens/HomeScreens';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+      <HomeScreen/> 
       <StatusBar style="auto" />
     </View>
   );
