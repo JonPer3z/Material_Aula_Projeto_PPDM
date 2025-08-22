e2eedea93a57c818ccb16d1f003ebc764cac6ba3
JonPer3z <perez.joaopedro@gmail.com>
2025-08-21 23:40:30 -0300

    Teste

--- ControlCash/src/Screens/HomeScreens.js
+++ ControlCash/src/Screens/HomeScreens.js
@@ -1,36 +1,25 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import Footer from "../Components/Footer";
-import { COLORS } from '../Components/Constants/Colors';
-import TransactionModal from '../Components/TransactionModal';
+import { COLORS } from '../Components/Constants/Colors';;
 import Header from "../Components/Header";
 
 export default function HomeScreen() {
-  const [modalVisible, setModalVisible] = useState(false);
 
-  const handleOpenModal = () => {
-    setModalVisible(true);
-  };
 
-  const handleCloseModal = () => {
-    setModalVisible(false);
-  };
+
 
   return (
     <SafeAreaView style={styles.screenContainer}>
 
       <Header />
 
-      <TransactionModal
-        visible={modalVisible}
-        onClose={handleCloseModal}
-      />
+
 
       <View style={styles.contentContainer}>
         <Text style={styles.text}>CONTEÚDO PRINCIPAL DA TELA</Text>
       </View>
 
-      <Footer onTransactionPress={handleOpenModal} />
+     
 
     </SafeAreaView>
   );
